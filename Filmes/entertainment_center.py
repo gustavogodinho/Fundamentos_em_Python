781091d6c6430e22231d590ee0d8857e0c47d86d
Gustavo Gilberto Godinho <11860941+gustavogodinho@users.noreply.github.com>
2019-01-03 16:06:39 -0200

    Add files via upload

--- Filmes/entertainment_center.py
+++ Filmes/entertainment_center.py
@@ -12,5 +12,23 @@ avatar = media.Movie("Avatar",
                         "https://100grana.files.wordpress.com/2009/11/avatar_french.jpg",
                         "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-movies = [toy_story, avatar]
+breakingBed = media.Movie("Breaking Bed",
+                          "Teste",
+                          "https://http2.mlstatic.com/poster-cartaz-breaking-bad-a-40x60cm-D_NQ_NP_798928-MLB25905656574_082017-F.jpg",
+                          "https://www.youtube.com/watch?v=ceqOTZnhgY8")
+
+escola_de_Rock = media.Movie("Escola de Rock",
+                          "Teste",
+                          "http://br.web.img3.acsta.net/c_215_290/medias/nmedia/18/91/90/98/20169244.jpg",
+                          "https://www.youtube.com/watch?v=-7zHtRk6pcI")
+
+narcos = media.Movie("Narcos",
+                          "Teste",
+                          "https://img.elo7.com.br/product/zoom/1E0EB0A/poster-cartaz-narcos-netflix-poster-tematico.jpg",
+                          "https://www.youtube.com/watch?v=U7elNhHwgBU")
+
+
+
+
+movies = [toy_story, avatar, breakingBed, escola_de_Rock, narcos]
 fresh_tomatoes.open_movies_page(movies)
